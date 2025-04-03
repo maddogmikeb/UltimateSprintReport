24a80f01a464ee2ea0f649bee0c2096eb15b7b11
Mike Burns <maddogmikeb@users.noreply.github.com>
2025-04-03 05:03:50 +0000

    build again

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,7 +11,7 @@ import pandas as pd
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.UltimateJiraSprintReport import UltimateJiraSprintReport
+from UltimateJiraSprintReport import UltimateJiraSprintReport
 
 class TestUltimateSprintReport(unittest.TestCase):
 
