365e8d086c8525b0d9e71985f1212953f5310617
Mike Burns <maddogmikeb@users.noreply.github.com>
2025-04-03 04:57:25 +0000

    build try again

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 # This file initializes the UltimateJiraSprintReport package. 
 
-from src.UltimateJiraSprintReport import UltimateJiraSprintReport
+from UltimateJiraSprintReport import UltimateJiraSprintReport
 
 __all__ = ["UltimateJiraSprintReport"]  # Define the public API of the package
